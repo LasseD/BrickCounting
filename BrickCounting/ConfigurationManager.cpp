25dac9c72f8f4ae82c6f1e2f7dcee7f7e32cf87b
Lasse Deleuran <lassedeleuran@gmail.com>
2015-09-10 09:23:12 +0200

    Changed testing method to focus on combination type 1/1/1/1/1/1.

--- BrickCounting/ConfigurationManager.cpp
+++ BrickCounting/ConfigurationManager.cpp
@@ -242,16 +242,19 @@ ConfigurationManager::ConfigurationManager(int maxSccSize, bool findExtremeAngle
 
 void ConfigurationManager::test() {
   std::vector<int> v;
-  v.push_back(2);
-  v.push_back(2);
-  v.push_back(2);
+  v.push_back(1);
+  v.push_back(1);
+  v.push_back(1);
+  v.push_back(1);
+  v.push_back(1);
+  v.push_back(1);
 
-  //runForCombinationType(v, 5);
-  
+  runForCombinationType(v, 5);
+/*  
   std::vector<FatSCC> v2;
   v2.push_back(sccs[1][0]);
   v2.push_back(sccs[1][4]);
-  v2.push_back(sccs[1][9]); // maybe 9, 10, 11, ...
+  v2.push_back(sccs[1][10]); // maybe 9, 10, 11, ...
   //ConfigurationEncoder encoder(v2);
 
   std::ofstream os;
